9b1843266582cb985da69c6759b4398488ed6522
Yusuf Karabay <yusufkarabay21@gmail.com>
2022-09-05 10:43:12 +0300

    ASBranch-1: Core layer is complete.

---
+++ AuthServer.Core/Services/IAauthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using AuthServer.Core.Dto;
+using AuthServer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAauthenticationService
+    {
+
+        //access token oluşturur
+        Task<CustomResponseDto<TokenDto>> CreateAccesTokenAsync(LoginDto loginDto);
+
+        //refresh token oluşturur
+        Task<CustomResponseDto<TokenDto>> CreateTokenByRefreshTokenAsync(string refreshToken);
+
+        //bu kullanıcı için refresh token varsa null yap şeklinde kodlayacağız
+        Task<CustomResponseDto<NoDataDto>> RevokeRefreshToken(string refreshToken); 
+
+        //client tipi az olduğu için appjsonda tutacağız. ordan gelen id ve secret kontrolünü burada işleyeceğiz
+        Task<CustomResponseDto<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
