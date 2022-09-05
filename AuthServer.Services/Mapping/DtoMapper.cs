6acdc096615592398bf1995ec6e5dd6b9046409e
Yusuf Karabay <yusufkarabay21@gmail.com>
2022-09-05 15:09:49 +0300

    ASBranch-3: Mapping folder and class creat

---
+++ AuthServer.Services/Mapping/DtoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Services.Mapping
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<UserApp, UserAppDto>().ReverseMap();
+        }
+    }
+}
